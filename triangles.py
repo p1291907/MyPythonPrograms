8a08388a4a73cd83fcfb517ff3605ec65b208672
p1291907 <67346987+p1291907@users.noreply.github.com>
2025-11-12 14:27:27 -0600

    Add files via upload

---
+++ triangles.py
@@ -0,0 +1,9 @@
+# multiplying strings to integers are allowed because it means that the character will be repeated
+# This program will draw two triangles one inverted
+# and one standard
+print("Inverted triangle")
+for counter in reversed(range(1,10)):
+    print("*" * counter)
+print("Right Triangle")
+for counter in range(1,5):
+    print("*" * counter)
